8aadf624e5d8f579a584da6a81f2539b10ebd697
Vladimir37 <vladimir37work@gmail.com>
2016-12-20 16:49:57 +0300

    News 0.1

--- backend/app/controllers/news.js
+++ backend/app/controllers/news.js
@@ -3,7 +3,44 @@
 var Models = require('../models/main');
 
 class NewsController {
-    getAllNews(req, res, next) {
+    index(req, res, next) {
+        Models.news.find().then((news) => {
+            news.reverse();
+            res.render('news/index', {
+                news,
+                err: req.query.err
+            });
+        }).catch((err) => {
+            res.send(this.responseFormat(err, true));
+        });
+    }
+
+    addPage(req, res, next) {
+        res.render('news/add');
+    }
+
+    editPage(req, res, next) {
+        res.render('news/edit');
+    }
+
+    addAction(req, res, next) {
+        Models.news.create({
+            title: req.data.title,
+            cover: req.data.cover,
+            text: req.data.text,
+            date: new Date()
+        }).then(() => {
+            res.redirect('/news/');
+        }).catch(() => {
+            res.redirect('/news/?err=500');
+        });
+    }
+
+    editAction(req, res, next) {
+        //
+    }
+
+    deleteAction(req, res, next) {
         //
     }
 }
--- backend/app/router/index.js
+++ backend/app/router/index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var index = require('../controllers/index');
 var auth = require('./auth');
 var api = require('./api');
+var news = require('./news');
 
 var router = express.Router();
 
@@ -11,6 +12,6 @@ router.get('/', index.index);
 router.get('/login', index.login);
 
 router.use('/auth', auth);
-router.use('/api', api);
+router.use('/news', news);
 
 module.exports = router;
